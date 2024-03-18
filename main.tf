3b1053250bf5effe53afad52bc01419f6efbdb05
AnantD95 <59355088+AnantD95@users.noreply.github.com>
2024-03-18 13:01:15 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,201 @@
+# Configure the Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Include variables from variables.tf
+terraform {
+  required_version = ">= 0.13"
+}
+
+# # Include variables from variables.tf
+# variable "resource_group_name" {}
+
+# variable "location" {}
+
+# variable "vm_name" {}
+
+# variable "vm_size" {}
+
+# variable "admin_username" {}
+
+# variable "admin_password" {}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.resource_group_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create a subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create a public IP address
+resource "azurerm_public_ip" "publicip" {
+  name                = "${var.vm_name}-publicip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+# Create a network security group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.vm_name}-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create a network interface
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "nicConfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
+# Create a virtual machine
+resource "azurerm_virtual_machine" "vm" {
+  name                  = var.vm_name
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = var.vm_size
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "${var.vm_name}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  os_profile {
+    computer_name  = var.vm_name
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_windows_config {
+    enable_automatic_upgrades = true
+    provision_vm_agent        = true
+  }
+}
+
+# Create a storage account
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Create a container within the storage account
+resource "azurerm_storage_container" "data-storage" {
+  name                  = var.storage-account-container
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+# Upload a file to the storage account
+resource "azurerm_storage_blob" "blob" {
+  name                   = var.blob
+  storage_account_name   = azurerm_storage_account.storage.name
+  storage_container_name = azurerm_storage_container.data-storage.name
+  type                   = "Block"
+  source                 = "D:/test/test.txt"  # Local path to file you want to upload
+}
+
+
+# Create an Azure VM extension to run PowerShell script
+resource "azurerm_virtual_machine_extension" "script_extension" {
+  name                 = "install-iis"
+  virtual_machine_id   = azurerm_virtual_machine.vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"
+
+  protected_settings = <<SETTINGS
+  {
+    "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(data.template_file.script.rendered)}')) | Out-File -filepath script.ps1\" && powershell -ExecutionPolicy Unrestricted -File script.ps1 -verb runas"
+  }
+  SETTINGS
+
+#   settings = <<SETTINGS
+#     {
+#         "commandToExecute": "powershell.exe -ExecutionPolicy Unrestricted -File script.ps1"
+#     }
+# SETTINGS
+
+#   protected_settings = <<PROTECTED_SETTINGS
+#     {
+#         "storageAccountName": "${azurerm_storage_account.storage.name}",
+#         "storageAccountKey": "${azurerm_storage_account.storage.primary_access_key}"
+#     }
+# PROTECTED_SETTINGS
+}
+
+data "template_file" "script" {
+  template = "${file("script.ps1")}"
+}
---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "resource_group_name" {
+  description = "The name of the Azure resource group."
+}
+
+variable "location" {
+  description = "The Azure region where resources will be provisioned."
+  default     = "East US"
+}
+
+variable "vm_name" {
+  description = "The name of the Azure VM."
+  default     = "my-windows-vm"
+}
+
+variable "vm_size" {
+  description = "The size of the Azure VM."
+  default     = "Standard_DS1_v2"
+}
+
+variable "admin_username" {
+  description = "The admin username for the Azure VM."
+  default     = "adminuser"
+}
+
+variable "admin_password" {
+  description = "The admin password for the Azure VM."
+  type        = string
+}
+
+variable "storage_account_name" {
+  default = "testdnsaccount"
+}
+
+variable "storage-account-container" {
+  default = "data-storage"
+}
+
+variable "blob" {
+  default = "test_dna"
+}
